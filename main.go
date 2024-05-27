45c3be611c28619ccbc4b538bdc0089c065d5bb2
daedalus <manny.is.daedalus@pm.me>
2024-05-27 21:22:58 +0000

    fix: proper db initialisation and handling

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	var db *sql.DB
-
-	_, err := os.Stat("./alexandria.db")
+	db, err := sql.Open("sqlite3", "./alexandria.db")
 	if err != nil {
-		db, err = sql.Open("sqlite3", "./alexandria.db")
-		if err != nil {
-			log.Fatalf("open err: %s", err.Error())
-		}
-		defer db.Close()
+		log.Fatalf("open err: %s", err.Error())
+	}
+	defer db.Close()
 
+	// check if the users table exists before creating it
+	if !tableExists(db, "users") {
 		err = createUsersTable(db)
 		if err != nil {
 			log.Fatalf("user table create err: %s", err.Error())
 		}
+	}
 
+	// check if the books table exists before creating it
+	if !tableExists(db, "books") {
 		err = createBooksTable(db)
 		if err != nil {
 			log.Fatalf("book table create err: %s", err.Error())
 		}
 
+		// populate books table only if it's newly created
 		books, err := getAllBooks()
 		if err != nil {
 			log.Fatal(err)
@@ -47,7 +47,19 @@ func main() {
 	m := initialModel(db)
 
 	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatalf("tea program err: %s", err.Error())
 	}
+}
+
+// function to check if a table exists in the database
+func tableExists(db *sql.DB, tableName string) bool {
+    // https://www.quora.com/How-can-you-check-if-a-table-exists-in-SQLite
+    query := `SELECT name FROM sqlite_master WHERE type='table' AND name=?`
+	rows, err := db.Query(query, tableName)
+	if err != nil {
+		log.Fatalf("error checking if table exists: %s", err.Error())
+	}
+	defer rows.Close()
+
+	return rows.Next()
 }
